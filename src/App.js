5ccf017ed370f7e6f7352f8734bce9684a484eeb
Giovanni Puntil <puntilgiovanni@gmail.com>
2017-05-02 16:47:51 +0100

    status.srl - a serious mood picker

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor () {
+    super();
+    this.state = { 'color': 'Pick one', status: 'Pick one' };
+  }
+  render() {
+    return (
+      <div className="App">
+        <div id="traffic-light" className={this.state.color}>
+          <h1>{this.state.status}</h1>
+        </div>
+        <ul id="color-selector">
+          <li><button className="green" onClick={()=>this.setState({'color': 'green', status: 'green :)'})}>GREEN</button></li>
+          <li><button className="amber" onClick={()=>this.setState({'color': 'amber', status: 'amber :|'})}>AMBER</button></li>
+          <li><button className="red" onClick={()=>this.setState({'color': 'red', status: 'red :('})}>RED</button></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
